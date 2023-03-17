c0d5ed9328cad955800842606834e76706ec2a39
adelinashahaj <adelinarucaj@icloud.com>
2023-03-17 15:57:30 +0100

    v-for incompleto

--- js/main.js
+++ js/main.js
@@ -3,10 +3,11 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
+        
         contacts: [
             {
             name: 'Michele',
-            avatar: './img/avatar_1.png',
+            avatar: '/img/avatar_1.png',
             visible: true,
             messages: [
             {
@@ -28,7 +29,7 @@ createApp({
                 },
                 {
                 name: 'Fabio',
-                avatar: './img/avatar_2.png',
+                avatar: '/img/avatar_2.png',
                 visible: true,
                 messages: [
                 {
@@ -50,7 +51,7 @@ createApp({
                 },
                 {
                 name: 'Samuele',
-                avatar: './img/avatar_3.png',
+                avatar: '/img/avatar_3.png',
                 visible: true,
                 messages: [
                 {
@@ -72,7 +73,7 @@ createApp({
                 },
                 {
                 name: 'Alessandro B.',
-                avatar: './img/avatar_4.png',
+                avatar: '/img/avatar_4.png',
                 visible: true,
                 messages: [
                 {
@@ -89,7 +90,7 @@ createApp({
                 },
                 {
                 name: 'Alessandro L.',
-                avatar: './img/avatar_5.png',
+                avatar: '/img/avatar_5.png',
                 visible: true,
                 messages: [
                 {
@@ -106,7 +107,7 @@ createApp({
                 },
                 {
                 name: 'Claudia',
-                avatar: './img/avatar_5.png',
+                avatar: '/img/avatar_5.png',
                 visible: true,
                 messages: [
                 {
@@ -127,7 +128,7 @@ createApp({
                 ],
                 },
                 {name: 'Federico',
-                avatar: './img/avatar_7.png',
+                avatar: '/img/avatar_7.png',
                 visible: true,
                 messages: [
                 {
@@ -144,7 +145,7 @@ createApp({
                 },
                 {
                 name: 'Davide',
-                avatar: './img/avatar_8.png',
+                avatar: '/img/avatar_8.png',
                 visible: true,
                 messages: [
                 {
